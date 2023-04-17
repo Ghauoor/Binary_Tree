294727f56989a15ca804c9b8a0e8811973428242
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-04-17 17:52:39 +0500

    Implement Post / Pre / In Order Traversal on Binary Tree

--- src/Implementation/BinaryTreeLL.java
+++ src/Implementation/BinaryTreeLL.java
@@ -8,12 +8,38 @@ public class BinaryTreeLL {
     }
 
     //PreOrder Traversal
+    //TC: --> O(N)
+    //SC: --> O(N)
     public void preOrder(BinaryNode node) {
         //Base case
         if (node == null) return;
 
         System.out.print(node.value + " ");
-        preOrder(node.left);
-        preOrder(node.right);
+        preOrder(node.left); //TC --> O(N / 2)
+        preOrder(node.right); //TC --> O(N / 2)
+    }
+
+    //inOrder Traversal
+    //TC: --> O(N)
+    //SC: --> O(N)
+    public void inOrder(BinaryNode node) {
+        //Base case
+        if (node == null) return;
+
+        inOrder(node.left); //TC --> O(N / 2)
+        System.out.print(node.value + " ");
+        inOrder(node.right); //TC --> O(N / 2)
+    }
+
+    //PostOrder Traversal
+    //TC: --> O(N)
+    //SC: --> O(N)
+    public void postOrder(BinaryNode node) {
+        //Base case
+        if (node == null) return;
+
+        postOrder(node.left); //TC --> O(N / 2)
+        postOrder(node.right); //TC --> O(N / 2)
+        System.out.print(node.value + " ");
     }
 }
