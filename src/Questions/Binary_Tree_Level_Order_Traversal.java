5938f3bfc42312f699b3428aa6a65e24d1f3648b
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-05-20 20:25:09 +0500

    102. Binary Tree Level Order Traversal

--- src/Questions/Binary_Tree_Level_Order_Traversal.java
+++ src/Questions/Binary_Tree_Level_Order_Traversal.java
@@ -17,6 +17,8 @@ public class Binary_Tree_Level_Order_Traversal {
             List<Integer> level = new ArrayList<>(levelSize);
             for (int i = 0; i < levelSize; i++) {
                 TreeNode cur = q.poll();
+                //// curr can be null so add null check alongside pushing
+                //// node to Queue
                 if (cur != null && cur.left != null) q.add(cur.left);
                 if (cur != null && cur.right != null) q.add(cur.right);
                 if (cur != null) {
@@ -29,4 +31,31 @@ public class Binary_Tree_Level_Order_Traversal {
         return res;
 
     }
+
+    //
+    public List<List<Integer>> levelOrderTraversal(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        List<List<Integer>> wrapList = new ArrayList<>();
+
+        if (root == null) return wrapList;
+        queue.offer(root);
+
+        while (!queue.isEmpty()) {
+            int levelNum = queue.size();
+            List<Integer> subList = new LinkedList<>();
+
+            for (int i = 0; i < levelNum; i++) {
+                if (queue.peek().left != null) {
+                    queue.offer(queue.peek().left);
+                }
+                if (queue.peek().right != null) {
+                    queue.offer(queue.peek().right);
+                }
+                subList.add(queue.poll().val);
+            }
+            wrapList.add(subList);
+        }
+        return wrapList;
+    }
+
 }
