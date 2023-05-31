e5ee25363f69248e54b562715084919ceef9a11c
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2023-05-31 18:21:56 +0500

    110. Balanced Binary Tree

--- src/Questions/Check_Balance_Binary_Tree.java
+++ src/Questions/Check_Balance_Binary_Tree.java
@@ -19,4 +19,24 @@ public class Check_Balance_Binary_Tree {
 
         return left && right && diff <= 1;
     }
+
+    // Better Approach
+
+    public boolean isBalancedTree(TreeNode root)  {
+        return dfsHeight(root) != -1;
+    }
+
+    int dfsHeight(TreeNode root) {
+        if (root == null) return 0;
+
+        int leftHeight = dfsHeight(root.left);
+        if (leftHeight == -1) return -1;
+        int rightHeight = dfsHeight(root.right);
+        if (rightHeight == -1) return -1;
+
+        if (Math.abs(leftHeight - rightHeight) > 1) return -1;
+        return Math.max(leftHeight, rightHeight) + 1;
+    }
+
+
 }
